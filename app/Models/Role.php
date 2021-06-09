9a348ca476469181c2087db0b1ecab7586a65101
januarelsan <januarelsan@gmail.com>
2021-06-09 19:46:26 +0700

    Add Role Model

---
+++ app/Models/Role.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    
+
+    public function users()
+    {
+        return $this->belongsToMany(
+            User::class,
+            'user_roles',
+            'user_id',
+            'role_id');
+    }
+}
